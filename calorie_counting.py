43df3f9f1a835f4f892311532e3b5355a15bb766
Simone Roscio <84132680+shilux42@users.noreply.github.com>
2022-12-03 17:31:28 +0100

    Add files via upload

---
+++ calorie_counting.py
@@ -0,0 +1,21 @@
+with open('day1/test_input.txt') as rations:
+
+    calories_list = []
+
+    for line in rations:
+        calories_list.append(line.replace('\n', ''))
+
+    ration_list = []
+    calories_sum = []
+
+    for el in calories_list:
+        if el != '':
+            el = int(el)
+            calories_sum.append(el)
+
+        if el == '':
+            ration = sum(calories_sum)
+            ration_list.append(ration)
+            calories_sum = []
+
+    print(ration_list)
